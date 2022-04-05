85a751cbf5077ca317809b3f7a78ff042f744960
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2022-04-05 20:38:42 +0200

    added functionallity for flying the star fighter

--- Source/FlightGame/StarFighter.cpp
+++ Source/FlightGame/StarFighter.cpp
@@ -2,14 +2,24 @@
 
 
 #include "StarFighter.h"
+#include <Runtime/Engine/Classes/GameFramework/SpringArmComponent.h>
+#include <Runtime/Engine/Classes/Camera/CameraComponent.h>
 
 // Sets default values
 AStarFighter::AStarFighter()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
 	RootComponent = root;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(root);
+	CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character
+
+	// Create a follow camera
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComp->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
 
 }
 
@@ -17,24 +27,112 @@ AStarFighter::AStarFighter()
 void AStarFighter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
 void AStarFighter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	auto newVelocity = GetLinearVelocityChange(
+		DeltaTime,
+		throttle < 0.f ? -velAccelerationSpeed : velAccelerationSpeed,
+		velDecelerationSpeed,
+		FMath::IsNearlyZero(throttle) == false);
+
+	moveVelocity = FMath::Clamp(moveVelocity + newVelocity, 0.1f, 1.f);
+	//UE_LOG(LogTemp, Warning, TEXT("%f velocity"), moveVelocity);
+
 	FVector location = GetActorLocation();
-
-	location += GetActorForwardVector() * 100.0f * DeltaTime;
-
+	location += GetActorForwardVector() * (MaxSpeed * DeltaTime * moveVelocity);
 	SetActorLocation(location);
+
+	auto pitchVel = GetPropotionalVelocityChange(
+		DeltaTime,
+		pitchVelocity,
+		turnAccelerationSpeed,
+		turnDecelerationSpeed,
+		bUpdatePitchVel);
+	pitchVelocity = FMath::Clamp(pitchVelocity + pitchVel, 0.f, 1.f);
+	auto updatedPitch = pitch * pitchSpeed * pitchVelocity;
+
+	auto rollVel = GetPropotionalVelocityChange(
+		DeltaTime,
+		rollVelocity,
+		turnAccelerationSpeed,
+		turnDecelerationSpeed,
+		bUpdateRollVel);
+	rollVelocity = FMath::Clamp(rollVelocity + rollVel, 0.f, 1.f);
+	auto updatedRoll = roll * rollSpeed * rollVelocity;
+
+
+	FRotator newRotation = FRotator(updatedPitch, 0.0f, updatedRoll) * DeltaTime;
+
+	FQuat quatRotation = FQuat(newRotation);
+
+	AddActorLocalRotation(quatRotation, false, 0, ETeleportType::None);
+
+}
+
+float AStarFighter::GetLinearVelocityChange(float deltaTime, float accelSpeed, float decelSpeed, bool changeCondition)
+{
+	auto velocityChange = (changeCondition ? deltaTime * accelSpeed : -(deltaTime * decelSpeed));
+	return velocityChange;
+}
+
+float AStarFighter::GetPropotionalVelocityChange(float deltaTime, float currentVelocity, float accelSpeed, float decelSpeed, bool changeCondition)
+{
+	auto proportionalDec = -(deltaTime + (deltaTime * (decelSpeed * currentVelocity)));
+	auto deceleration = moveVelocity > .0f ? proportionalDec : -deltaTime;
+	auto velocityChange = (changeCondition ? deltaTime * accelSpeed : deceleration);
+	return velocityChange;
 }
 
 // Called to bind functionality to input
 void AStarFighter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	InputComponent->BindAxis("Throttle", this, &AStarFighter::ReadThrottle);
+	InputComponent->BindAxis("Roll", this, &AStarFighter::ReadRoll);
+	InputComponent->BindAxis("Pitch", this, &AStarFighter::ReadPitch);
 }
+
+void AStarFighter::ReadThrottle(float value)
+{
+	throttle = value;
+}
+
+void AStarFighter::ReadRoll(float value)
+{
+	if (FMath::IsNearlyZero(value))
+	{
+		bUpdateRollVel = false;
+		return;
+	}
+	//TODO if differnce between value and roll is greater than 0.3 then reset velocity
+	/*if ((roll > 0.f && value < 0.f) || (roll < 0.f && value > 0.f))
+	{
+		rollVelocity = 0.f;
+	}*/
+	roll = value;
+	bUpdateRollVel = true;
+}
+
+void AStarFighter::ReadPitch(float value)
+{
+	if (FMath::IsNearlyZero(value))
+	{
+		bUpdatePitchVel = false;
+		return;
+	}
+	/*if ((pitch > 0.f && value < 0.f) || (pitch < 0.f && value > 0.f))
+	{
+		pitchVelocity = 0.f;
+	}*/
+	pitch = -value;
+	bUpdatePitchVel = true;
+}
+
+
 
--- Source/FlightGame/StarFighter.h
+++ Source/FlightGame/StarFighter.h
@@ -18,14 +18,56 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	
+	float GetLinearVelocityChange(float deltaTime, float accelSpeed, float decelSpeed, bool changeCondition);
+
+	float GetPropotionalVelocityChange(float deltaTime, float currentVelocity, float accelSpeed, float decelSpeed, bool changeCondition);
+
+	void ReadThrottle(float value);
+
+	void ReadRoll(float value);
+
+	void ReadPitch(float value);
+
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	USceneComponent* root;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* CameraComp;
+
+	UPROPERTY(EditDefaultsOnly)
+	float MaxSpeed = 10000.0f;
+
+	UPROPERTY(BlueprintReadOnly)
+	float moveVelocity = .0f;
+	float velDecelerationSpeed = 0.01f;
+	float velAccelerationSpeed = 0.3f;
+	float turnDecelerationSpeed = 3.f;
+	float turnAccelerationSpeed = 4.f;
+
+	float throttle = .0f;
+	float roll = .0f;
+	float pitch = .0f;
+	float rollSpeed = 100.f;
+	float pitchSpeed = 75.f;
+
+	UPROPERTY(BlueprintReadOnly)
+	float rollVelocity = .0f;
+	UPROPERTY(BlueprintReadOnly)
+	float pitchVelocity = .0f;
+
+	bool bUpdateRollVel = false;
+	bool bUpdatePitchVel = false;
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
